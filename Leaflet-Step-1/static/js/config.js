7c968a2b7592947217ae89a2ba3aa0658bb6ac2e
S-Broberg <78188204+S-Broberg@users.noreply.github.com>
2021-06-09 14:51:28 -0700

    Update config.js

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYXZpbGFsaXpiZXRoIiwiYSI6ImNrZnUxOWkybzBucXkyenFxZDBxcmx3dTcifQ.sSUxYb70FGDUea9QWkSXdQ"
+const API_KEY = "pk.eyJ1Ijoic2JybyIsImEiOiJja3BxMDVtN2MwN21lMzBwN3A0ajQ2ZWxqIn0.nkcgiqJdDoo90Ax8Wyb8dw"
